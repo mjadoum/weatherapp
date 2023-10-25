aa190a4a643dc359e000e4b6d2b7911451a1467d
Mustafa-Jadoun <54470645+mjadoum@users.noreply.github.com>
2023-10-25 15:37:18 +0100

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-// vue.config.js file to be placed in the root of your repository
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+ 
+  transpileDependencies: true
+})
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/WEATHER-APP/'
-    : '/'
-}
+
